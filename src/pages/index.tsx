6941ae8b36d568b3ea5c8546e8d367899f64064b
Joseph Dlc Rivas <razorij20@gmail.com>
2023-06-05 03:30:37 -0500

    change home title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import { MainLayout } from '@/Layouts/MainLayout'
 export default function HomePage() {
   return (
     <MainLayout>
-      <h1>Home Page</h1>
+      <h1>Home Page rkfmjrfjt</h1>
     </MainLayout>
   )
 }
